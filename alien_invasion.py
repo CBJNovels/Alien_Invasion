96f7d71db8c841f319e3895603e45a4698baee4b
CBJNovels <2898901843@qq.com>
2018-07-26 00:01:44 +0800

    Training_1

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ def run_game():
         #监视键盘和鼠标事件
         gf.check_events(ship)
         #每次循环都重绘屏幕
+        ship.update()
         gf.update_screen(ai_settings,screen,ship)
 
         #让最近绘制的屏幕可见
